e29c0d0b55d75b1037ff4b674edbfd8150eee7bb
Raphael Picard <raphaelpicard@outlook.fr>
2021-10-27 16:14:04 +0200

    feat #1 : Added routes to the server

--- back/index.js
+++ back/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+const { routes } = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
     port: process.env.STAR_WARS_API_PORT,
     host: process.env.STAR_WARS_API_HOST,
   });
-
+  server.route(routes);
   try {
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
