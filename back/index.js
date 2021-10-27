6638e3b057ddbf24af9f281b783d13fe391e0393
Raphael Picard <raphaelpicard@outlook.fr>
2021-10-27 09:23:27 +0200

    feat #5 : Setting up the server with Hapi

---
+++ back/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.STAR_WARS_API_PORT,
+    host: process.env.STAR_WARS_API_HOST,
+  });
+
+  try {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
