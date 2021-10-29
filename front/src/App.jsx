f72c8e22b592e0cf65dcfb4a97aca4c8f278072f
Raphael Picard <raphaelpicard@outlook.fr>
2021-10-29 07:27:59 +0200

    feat #2 : Added styles and component Header/CssCircularProgress

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Switch, Route } from "react-router-dom";
+import CssCircularProgress from "./components/CssCircularProgress";
+import Header from "./components/Header";
+import "./styles/styles.css";
+import "./styles/colors.css";
 
-const App = ({ routes }) => {
-  console.log(routes);
-  return (
-    <React.Suspense fallback={<h1>Loading...</h1>}>
-      <Switch>
-        {routes.map((route) => (
-          <Route
-            key={route.key}
-            exact={route.exact}
-            path={route.path}
-            render={(props) => {
-              const Component = React.lazy(() =>
-                import(`./views/containers/${route.component}Container`)
-              );
-              return <Component {...props} />;
-            }}
-          />
-        ))}
-      </Switch>
-    </React.Suspense>
-  );
-};
+const App = ({ routes }) => (
+  <React.Suspense fallback={<CssCircularProgress />}>
+    <Switch>
+      {routes.map((route) => (
+        <Route
+          key={route.key}
+          exact={route.exact}
+          path={route.path}
+          render={(props) => {
+            const Component = React.lazy(() =>
+              import(`./views/containers/${route.component}Container`)
+            );
+            return (
+              <>
+                <Header />
+                <Component {...props} />
+              </>
+            );
+          }}
+        />
+      ))}
+    </Switch>
+  </React.Suspense>
+);
 
 App.propTypes = {
   routes: PropTypes.arrayOf(
