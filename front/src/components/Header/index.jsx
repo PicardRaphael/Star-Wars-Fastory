c118f9101cebbb77d378b33155cf541320c55728
Raphael Picard <raphaelpicard@outlook.fr>
2021-10-30 13:43:01 +0200

    feat #4 : Added Nav in Header

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import styles from "./Header.module.css";
 
 const Header = () => (
   <AppBar position="static" className={styles.appBar}>
-    <Toolbar>
+    <Toolbar className={styles.toolbar}>
       <Typography variant="h1" noWrap className={styles.title}>
         Star Wars Fastory
       </Typography>
+      <Box>
+        <NavLink exact activeClassName="active" className={styles.link} to="/">
+          Search
+        </NavLink>
+      </Box>
     </Toolbar>
   </AppBar>
 );
