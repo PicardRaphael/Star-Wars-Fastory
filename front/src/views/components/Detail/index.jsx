45c8b45086e2ff40e7b7119cd7e20fedafec2114
Raphael Picard <raphaelpicard@outlook.fr>
2021-10-30 13:45:45 +0200

    feat #4 : Added Detail with all type

--- front/src/views/components/Detail/index.jsx
+++ front/src/views/components/Detail/index.jsx
@@ -1,5 +1,58 @@
-const Detail = () => {
-  return <h1>Detail</h1>;
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { v4 as uuidv4 } from "uuid";
+import CssCircularProgress from "../../../components/CssCircularProgress";
+import styles from "./Detail.module.css";
+
+const People = React.lazy(() =>
+  import("../../../containers/Detail/PeopleContainer")
+);
+const Film = React.lazy(() =>
+  import("../../../containers/Detail/FilmContainer")
+);
+const Planet = React.lazy(() =>
+  import("../../../containers/Detail/PlanetContainer")
+);
+const Species = React.lazy(() =>
+  import("../../../containers/Detail/SpeciesContainer")
+);
+const Starship = React.lazy(() =>
+  import("../../../containers/Detail/StarshipContainer")
+);
+const Vehicle = React.lazy(() =>
+  import("../../../containers/Detail/VehicleContainer")
+);
+const Detail = ({ loading, result, type }) => {
+  const types = {
+    people: People,
+    films: Film,
+    planets: Planet,
+    species: Species,
+    starships: Starship,
+    vehicles: Vehicle,
+  };
+  const Component = types[type];
+  return (
+    <Box>
+      {(loading || !result) && <CssCircularProgress />}
+      {!loading && result && (
+        <Grid
+          container
+          justifyContent="center"
+          className={styles.gridContainer}
+        >
+          {result.map((item) => (
+            <React.Suspense key={uuidv4()} fallback={<CssCircularProgress />}>
+              <Grid xl={6} xs={12} item className={styles.gridItem}>
+                <Component result={item} />
+              </Grid>
+            </React.Suspense>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Detail;
--- front/src/views/containers/DetailContainer/index.jsx
+++ front/src/views/containers/DetailContainer/index.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useDetailQuery } from "../../../api/apiSlice";
+
 import Detail from "../../components/Detail";
 
-const DetailContainer = () => <Detail />;
+const DetailContainer = () => {
+  const { type, name } = useParams();
+  const [result, setResult] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+  const { data, isFetching, isSuccess } = useDetailQuery({
+    type: type,
+    name: name,
+  });
+
+  useEffect(() => {
+    if (isFetching) {
+      setLoading(true);
+    } else if (isSuccess) {
+      setLoading(false);
+      setResult(data);
+    }
+  }, [data, isFetching, isSuccess]);
+
+  return <Detail result={result} loading={loading} type={type} />;
+};
 
 export default DetailContainer;
