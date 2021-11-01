10569bb94bec410d5279efd4fe429ab18990523c
Raphael Picard <raphaelpicard@outlook.fr>
2021-11-01 07:22:31 +0100

    feat #4 : Updated NotFound

--- front/src/views/components/NotFound/index.jsx
+++ front/src/views/components/NotFound/index.jsx
@@ -1,5 +1,20 @@
+import Box from "@mui/material/Box";
+import styles from "./NotFound.module.css";
+import notFound from "../../../assets/images/planet-404.png";
+import { Typography } from "@mui/material";
+
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return (
+    <Box className={styles.notFound}>
+      <Box className={styles.notFoundImage}>
+        <strong className={styles.number}>4</strong>
+        <img src={notFound} alt="not-found" loading="lazy" />
+        <strong className={styles.number}>4</strong>
+      </Box>
+      <Typography>THIS PAGE IS NOT FULLY ARMED AND OPERATIONAL.</Typography>
+      <Typography className={styles.try}>TRY SOMETHING ELSE?</Typography>
+    </Box>
+  );
 };
 
 export default NotFound;
